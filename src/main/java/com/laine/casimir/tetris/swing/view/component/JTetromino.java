d3b166553108c14da5eba35ac9ea421df4892608
Casimir Laine <casimir.laine@gmail.com>
2020-12-06 21:35:34 +0200

    Set background for JTetrominos.

--- src/main/java/com/laine/casimir/tetris/swing/view/component/JTetromino.java
+++ src/main/java/com/laine/casimir/tetris/swing/view/component/JTetromino.java
@@ -5,9 +5,10 @@ import com.laine.casimir.tetris.base.model.Tetromino;
 import com.laine.casimir.tetris.swing.SwingTetrisConstants;
 import com.laine.casimir.tetris.swing.view.layout.TetrisGridLayout;
 
+import javax.swing.*;
 import java.awt.*;
 
-public final class JTetromino extends AbstractTetrisComponent {
+public final class JTetromino extends JPanel {
 
     @Override
     public void setBackground(Color bg) {
@@ -22,11 +23,10 @@ public final class JTetromino extends AbstractTetrisComponent {
         if (tetromino == null) {
             return;
         }
-        setBackground(Color.decode(tetromino.getColorHex()));
         setLayout(new TetrisGridLayout(tetromino.getDimension(), tetromino.getDimension(), SwingTetrisConstants.TETROMINO_CELL_SIZE));
         for (final Position position : tetromino.getSquarePositions()) {
             final JTetrisSquare square = new JTetrisSquare();
-            square.setBackground(getBackground());
+            square.setBackground(Color.decode(tetromino.getColorHex()));
             add(square, new Point(position.getX(), position.getY()));
         }
         setFocusable(false);
