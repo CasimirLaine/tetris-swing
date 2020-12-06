4886429f72d0f5cb731be14a442de00ceb417894
Casimir Laine <casimir.laine@gmail.com>
2020-12-06 13:42:35 +0200

    JTetrisGamePanel will now treat all the squares the same way.

--- src/main/java/com/laine/casimir/tetris/swing/view/component/panel/JTetrisGamePanel.java
+++ src/main/java/com/laine/casimir/tetris/swing/view/component/panel/JTetrisGamePanel.java
@@ -1,7 +1,10 @@
 package com.laine.casimir.tetris.swing.view.component.panel;
 
 import com.laine.casimir.tetris.base.control.TetrisController;
-import com.laine.casimir.tetris.base.model.*;
+import com.laine.casimir.tetris.base.model.Playfield;
+import com.laine.casimir.tetris.base.model.Position;
+import com.laine.casimir.tetris.base.model.Square;
+import com.laine.casimir.tetris.base.model.Tetromino;
 import com.laine.casimir.tetris.swing.SwingTetrisConstants;
 import com.laine.casimir.tetris.swing.SwingTetrisSettings;
 import com.laine.casimir.tetris.swing.control.SwingKeyControls;
@@ -117,27 +120,14 @@ final class JTetrisGamePanel extends JPanel {
         for (int index = 0; index < tetrisSquares.size(); index++) {
             tetrisSquares.get(index).setBackground(transparent);
         }
-        final Playfield playfield = tetrisController.getPlayfield();
-        final List<Square> landedSquares = playfield.getLandedSquares();
-        for (int index = 0; index < landedSquares.size(); index++) {
-            final Square square = landedSquares.get(index);
+        final List<Square> allSquares = tetrisController.getAllSquares();
+        for (int index = 0; index < allSquares.size(); index++) {
+            final Square square = allSquares.get(index);
             if (square != null) {
                 final Position position = square.getPosition();
                 final int uiSquareIndex = tetrisGrid.getColCount() * position.getY() + position.getX();
                 if (uiSquareIndex >= 0 && uiSquareIndex < tetrisSquares.size()) {
                     tetrisSquares.get(uiSquareIndex).setBackground(Color.decode(square.getColorHex()));
-                }
-            }
-        }
-        final FallingTetromino fallingTetromino = playfield.getFallingTetromino();
-        if (fallingTetromino != null) {
-            final Tetromino tetromino = fallingTetromino.getTetromino();
-            final List<Position> positions = fallingTetromino.getTetromino().getSquarePositions();
-            final Position offset = fallingTetromino.getPosition();
-            for (Position position : positions) {
-                final int uiSquareIndex = tetrisGrid.getColCount() * (position.getY() + offset.getY()) + (position.getX() + offset.getX());
-                if (uiSquareIndex >= 0 && uiSquareIndex < tetrisSquares.size()) {
-                    tetrisSquares.get(uiSquareIndex).setBackground(Color.decode(tetromino.getColorHex()));
                 }
             }
         }
